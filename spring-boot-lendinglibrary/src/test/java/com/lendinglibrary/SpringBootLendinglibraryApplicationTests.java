3ebd7bf4d19c6d13b95e80c6082c240f3b7dec20
dimi09 <59616354+dimi09@users.noreply.github.com>
2021-09-22 00:21:06 +0300

    Add files via upload

---
+++ spring-boot-lendinglibrary/src/test/java/com/lendinglibrary/SpringBootLendinglibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendinglibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLendinglibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
